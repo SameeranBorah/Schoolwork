058c7761484b9be8dbc32236e90dea4de25bbfdd
John Frederick Lake <jlake2@student00.cse.nd.edu>
2015-02-23 12:28:42 -0500

    Getting started on the Python Primer

---
+++ cse30332/homework/python/primer/_movie_database.py
@@ -0,0 +1,42 @@
+#John F. Lake, Jr. 
+#Python Primer
+
+
+
+
+
+class _movie_database:
+
+	def __init__(self):
+		movies = {}
+		users = {}
+		ratings = {}
+
+
+
+
+	# Loads in a movies file
+	def load_movies(movie_file):
+		movies = {}
+		movieFile = open(movie_file)
+		for line in movieFile:
+			print line
+		
+	#def get_movie(mid):
+	#def movies():
+	##def set_movie(mid,list(title,genres)):
+	#def delete_movie(mid):
+	#def load_users(users_file):
+	##def get_user(uid):
+	##def get_users():
+	##def set_user(uid,list(gender,age,job,zip)):
+	##def delete_user(uid):
+	##def load_ratings(ratings_file):
+	##def get_rating(mid):
+	##def get_highest_rated_movie():
+	##def set_user_movie_rating(uid,mid,rating):
+	##def get_user_movie_rating(uid,mid):
+	#def delete_all_ratings():
+
+
+
