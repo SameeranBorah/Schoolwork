974f97130511efe0e7f78bab63ffaa5322462d4b
John Frederick Lake <jlake2@student00.cse.nd.edu>
2015-04-14 21:50:01 -0400

    Working on game sound

--- cse30332/homework/python/pygame/game.py
+++ cse30332/homework/python/pygame/game.py
@@ -19,29 +19,36 @@ class Earth(pygame.sprite.Sprite):
 class Player(pygame.sprite.Sprite):
 	def __init__(self,gs=None):
 		pygame.sprite.Sprite.__init__(self)
+		self.shootSound = pygame.mixer.Sound("explode.wav")
+		print self.shootSound.get_length()
+		self.shootSound.play()
 		self.gs = gs
 		self.image = pygame.image.load("deathstar.png")
 		self.rect = self.image.get_rect()
+		cent = self.rect.center
+		self.laser = pygame.image.load("laser.png")
+		self.laserrect = self.laser.get_rect(center=cent)
 		self.rect = self.rect.move(20,20)
 		self.orig_image = self.image
 		self.location = self.rect
 		self.tofire = False
+		self.laserX = 0
+		self.laserY = 0
+		self.velX = 0
+		self.velY = 0
 
 	def rotate(self):
 		mx,my = pygame.mouse.get_pos()
-		if self.tofire == True:
-			print "laser beam here."
-		else:
-			y = self.location.y + self.location.width/2
-			x = self.location.x + self.location.height/2
-			top = my-y
-			bottom = mx-x
-			if bottom == 0:
-				bottom = 0.0001
-			angle = math.degrees(math.atan2(top,bottom))+50
-			oldcenter = self.rect.center
-			self.image = pygame.transform.rotate(self.orig_image,angle*-1.0)
-			self.rect = self.image.get_rect(center=oldcenter)
+		y = self.location.y + self.location.width/2
+		x = self.location.x + self.location.height/2
+		top = my-y
+		bottom = mx-x
+		if bottom == 0:
+			bottom = 0.0001
+		angle = math.degrees(math.atan2(top,bottom))+50
+		oldcenter = self.rect.center
+		self.image = pygame.transform.rotate(self.orig_image,angle*-1.0)
+		self.rect = self.image.get_rect(center=oldcenter)
 
 
 
@@ -49,27 +56,59 @@ class Player(pygame.sprite.Sprite):
 		for event in pygame.event.get():
 			if event.type is pygame.KEYDOWN:
 				if event.key == pygame.K_LEFT:
-					print "LEFT"
-					self.rect = self.rect.move(-20,0)
+					self.velX = -5
 				elif event.key == pygame.K_RIGHT:
-					print "RIGHT"
-					self.rect = self.rect.move(20,0)
+					self.velX = 5
 				elif event.key == pygame.K_UP:
-					print "UP"
-					self.rect = self.rect.move(0,-20)
+					self.velY = -5
 				elif event.key == pygame.K_DOWN:
-					print "DOWN"
-					self.rect = self.rect.move(0,20)
-		self.location = self.rect
-		if self.location.x <=0:
-			self.location.x = 0
-		elif self.location.y <=0:
-			self.location.y = 0
-		elif self.location.y >= 480 - self.location.height:
-			self.location.y = 480-self.location.height
-		elif self.location.x >= 640 -self.location.width:
-			self.location.x = 640-self.location.width
-		self.rotate()
+					self.velY = 5
+			elif event.type is pygame.KEYUP:
+				if event.key == pygame.K_LEFT:
+					self.velX = 0
+				elif event.key == pygame.K_RIGHT:
+					self.velX = 0
+				elif event.key == pygame.K_UP:
+					self.velY = 0
+				elif event.key == pygame.K_DOWN:
+					self.velY = 0
+			elif event.type is pygame.MOUSEBUTTONDOWN:
+				self.tofire = True
+				self.center = self.rect.center 
+				self.laserrect = self.laser.get_rect(center=self.center)
+				self.laserdestX,self.laserdestY = pygame.mouse.get_pos()
+				self.laserX = self.laserrect.x
+				self.laserY = self.laserrect.y
+				self.laserDX = (self.laserdestX-self.laserX)/20
+
+				self.laserDY = (self.laserdestY-self.laserY)/20
+
+			elif event.type is pygame.MOUSEBUTTONUP:
+				self.tofire = False
+
+
+		if self.tofire == True:
+			#emit laser beam: 
+			
+			self.laserrect = self.laserrect.move(self.laserDX,self.laserDY)
+			self.shootSound.play()
+		else:
+
+			#Rotate the death start
+			self.location = self.rect
+			if self.location.x <=0:
+				self.location.x = 0
+			elif self.location.y <=0:
+				self.location.y = 0
+			elif self.location.y >= 480 - self.location.height:
+				self.location.y = 480-self.location.height
+			elif self.location.x >= 640 -self.location.width:
+				self.location.x = 640-self.location.width
+			self.rotate()
+
+			#Move the death star
+			self.rect = self.rect.move(self.velX,self.velY)
+
 			
 
 				
@@ -77,6 +116,7 @@ class GameSpace:
 	def main(self):
 		#Initialize everything:
 		pygame.init()
+		pygame.mixer.init()
 		self.size = self.width,self.height=640,480
 		self.black = 0,0,0
 		self.screen = pygame.display.set_mode(self.size)
@@ -90,6 +130,11 @@ class GameSpace:
 			self.player.tick()
 			self.screen.fill(self.black)
 			self.screen.blit(self.player.image,self.player.rect)
+			while self.player.tofire is True:
+				self.screen.blit(self.player.laser,self.player.laserrect)
+				self.screen.blit(self.earth.image,self.earth.rect)
+				self.player.tick()
+				pygame.display.flip()
 			self.screen.blit(self.earth.image,self.earth.rect)
 			pygame.display.flip()
 
