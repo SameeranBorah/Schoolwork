94441156b11a43fd51556aa5f3647c150331a7f0
John Frederick Lake <jlake2@student00.cse.nd.edu>
2015-04-23 19:28:06 -0400

    Got Twisted to work!

--- cse30332/homework/python/twisted/home.py
+++ cse30332/homework/python/twisted/home.py
@@ -1,21 +1,58 @@
 #John F. Lake, Jr. 
 #Home listens on port 9001 for a client (from work)
-
 from twisted.internet.protocol import Factory
 from twisted.protocols.basic import LineReceiver
 from twisted.internet import reactor
 
-class Command(LineReceiver):
+class Data(LineReceiver):
+	def dataReceived(self,data):
+		clf.getProt().sendData(data)
+	def sendData(self,arg):
+		self.transport.write(arg)
+	
+
+class DataFactory(Factory):
+	def __init__(self):
+		self.data = {}
+		self.prot = Data()
+	def buildProtocol(self, addr):
+		return self.prot
+	def getProt(self):
+		return self.prot
+
+class Client(LineReceiver):
 	def __init__(self, data):
 		self.data = data
 	def connectionMade(self):
-		print "Command Connected!"
+		#need command factor to send message
+		cf.getProt().sendData("data")
+	def dataReceived(self,data):
+		df.getProt().sendData(data)
+	def sendData(self,data):
+		self.transport.write(data)
+
+class ClientFactory(Factory):
+	def __init__(self):
+		self.data = {}
+		self.clProt = Client(self.data)
+	def buildProtocol(self, addr):
+		return self.clProt
+	def getProt(self):
+		return self.clProt
+
+
+class Command(LineReceiver):
+	def __init__(self, data):
+		self.data = data
 	def lineReceived(self, line):
 		self.sendLine(line)
-		print line
-	def sendData(self):
-		self.sendLine("data");
-
+	def sendData(self,arg):
+		if arg == "data":
+			self.sendLine(arg);
+			#listen on data port 
+			reactor.listenTCP(9323, df)
+		else:
+			df.getProt().sendData(arg)
 class CommandFactory(Factory):
 	def __init__(self):
 		self.data = {}
@@ -25,37 +62,16 @@ class CommandFactory(Factory):
 	def getProt(self):
 		return self.comProt
 
-class Client(LineReceiver):
-	def __init__(self, data):
-		self.data = data
-	def connectionMade(self):
-		print "SSH connected"
-		#need command factor to send message
-		cf.getProt().sendData()
-		
-	def lineReceived(self, line):
-		self.sendLine(line)
-		print line
-
-class ClientFactory(Factory):
-	def __init__(self):
-		self.data = {}
-		self.clProt = Client(self.data)
-	def buildProtocol(self, addr):
-		return self.clProt
-
-	def getProt(self):
-		return self.clProt
 
 
 cf = CommandFactory()
-
+df = DataFactory()
 clf = ClientFactory()
 
 
 #Listen for the work command connection
-reactor.listenTCP(9001, cf)
-reactor.listenTCP(9002, clf)
+reactor.listenTCP(9321, cf)
+reactor.listenTCP(9322, clf)
 
 reactor.run()
 
--- cse30332/homework/python/twisted/work.py
+++ cse30332/homework/python/twisted/work.py
@@ -3,25 +3,59 @@
 #This is the file that establishes the command connection
 from twisted.internet.protocol import Factory
 from twisted.internet.protocol import ClientFactory
+from twisted.protocols.basic import LineReceiver
 from twisted.internet.protocol import Protocol
 from twisted.internet import reactor
 
 
+class SSH(LineReceiver):
+	def dataReceived(self,data):
+		df.getProt().sendData(data)
+	def sendData(self,data):
+		self.transport.write(data)
+	
+
+class SSHFactory(ClientFactory):
+	def __init__(self):
+		self.prot = SSH()
+	def buildProtocol(self,addr):
+		return self.prot
+	def getProt(self):
+		return self.prot
+		
+
+class Data(LineReceiver):
+	def connectionMade(self):
+		reactor.connectTCP('student03.cse.nd.edu',22,ssh)
+
+	def dataReceived(self,data):
+		ssh.getProt().sendData(data)
+	def sendData(self,data):
+		self.transport.write(data)
+
+class DataFactory(ClientFactory):
+	def __init__(self):
+		self.prot = Data()
+	def buildProtocol(self,addr):
+		return self.prot
+	def getProt(self):
+		return self.prot
+
+
 class Command(Protocol):
 	def dataReceived(self,data):
-		print data
-	def connectionMade(self):
-		print "Connected!!!"
-		self.transport.write("This is from your work.!\r\n")
-	def connectionLost(self,data):
-		print "lost 'em"
+		#if they send "data", connect to data port
+		if data == "data\r\n":
+			reactor.connectTCP('student00.cse.nd.edu',9323,df)
 
 class CommandFactory(ClientFactory):
 	def buildProtocol(self,addr):
 		return Command()
 
+ssh = SSHFactory()
+df = DataFactory()
+
 
 #Connect on the command port
-reactor.connectTCP('student00.cse.nd.edu',9001,CommandFactory())
-reactor.connectTCP
+reactor.connectTCP('student00.cse.nd.edu',9321,CommandFactory())
 reactor.run()
