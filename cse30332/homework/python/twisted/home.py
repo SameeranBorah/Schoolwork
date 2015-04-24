e2243995751bd0b3e62ddcd6b8349992dd975cb3
John Frederick Lake <jlake2@student00.cse.nd.edu>
2015-04-23 21:18:07 -0400

    About to turn in

--- cse30332/homework/python/twisted/home.py
+++ cse30332/homework/python/twisted/home.py
@@ -1,86 +1,84 @@
 #John F. Lake, Jr. 
-#Home listens on port 9001 for a client (from work)
+#CSE30332
+#This should run on student00
+
+#PORTS:
+#9321: Command connection
+#9322: Client connection (when you call ssh on localhost of student00)
+#9323: Data connection
+
+#The command I use for SSH is "ssh localhost -p 9322 -l jlake2"
+
 from twisted.internet.protocol import Factory
 from twisted.protocols.basic import LineReceiver
 from twisted.internet import reactor
 
+#Data Protocol
 class Data(LineReceiver):
+	#Forward data to client connection
 	def dataReceived(self,data):
 		clf.getProt().sendData(data)
+	#Send data along data connection
 	def sendData(self,arg):
 		self.transport.write(arg)
-	
 
+#Factory for Data connection
 class DataFactory(Factory):
 	def __init__(self):
-		self.data = {}
 		self.prot = Data()
 	def buildProtocol(self, addr):
 		return self.prot
 	def getProt(self):
 		return self.prot
 
+#Client Protocol
 class Client(LineReceiver):
-	def __init__(self, data):
-		self.data = data
 	def connectionMade(self):
-		#need command factor to send message
+		#need command factory to send message
 		cf.getProt().sendData("data")
+	#Forward data to data connection: 
 	def dataReceived(self,data):
 		df.getProt().sendData(data)
+	#Send data to client connection
 	def sendData(self,data):
 		self.transport.write(data)
 
+#Factory for client connection
 class ClientFactory(Factory):
 	def __init__(self):
-		self.data = {}
-		self.clProt = Client(self.data)
+		self.clProt = Client()
 	def buildProtocol(self, addr):
 		return self.clProt
 	def getProt(self):
 		return self.clProt
 
-
+#Command protocol
 class Command(LineReceiver):
-	def __init__(self, data):
-		self.data = data
-	def lineReceived(self, line):
-		self.sendLine(line)
+	#How we establish the data connection: send "data" over command connection and listen
 	def sendData(self,arg):
 		if arg == "data":
 			self.sendLine(arg);
-			#listen on data port 
 			reactor.listenTCP(9323, df)
 		else:
 			df.getProt().sendData(arg)
+
+#Factory for command connection:
 class CommandFactory(Factory):
 	def __init__(self):
-		self.data = {}
-		self.comProt = Command(self.data)
+		self.comProt = Command()
 	def buildProtocol(self, addr):
 		return self.comProt
 	def getProt(self):
 		return self.comProt
 
-
-
+#Make all of the factory objects: 
 cf = CommandFactory()
 df = DataFactory()
 clf = ClientFactory()
 
-
 #Listen for the work command connection
 reactor.listenTCP(9321, cf)
+
+#Listen for the client connection (which will be ssh)
 reactor.listenTCP(9322, clf)
-
 reactor.run()
-
-
-
-
-
-
-
-
-
-
--- cse30332/homework/python/twisted/work.py
+++ cse30332/homework/python/twisted/work.py
@@ -1,20 +1,30 @@
 #John F. Lake, Jr. 
-#CSE 30332
-#This is the file that establishes the command connection
+#CSE30332
+#This can run on any machine (ideally student01 though)
+
+
+#PORTS:
+#9321: Command connection
+#9323: Data connection
+#22: SSH Connection (connects to student03)
+
+
 from twisted.internet.protocol import Factory
 from twisted.internet.protocol import ClientFactory
 from twisted.protocols.basic import LineReceiver
 from twisted.internet.protocol import Protocol
 from twisted.internet import reactor
 
-
+#SSH Protocol
 class SSH(LineReceiver):
+	#Forward data to data connection
 	def dataReceived(self,data):
 		df.getProt().sendData(data)
+	#Send data along SSH connection
 	def sendData(self,data):
 		self.transport.write(data)
 	
-
+#Factory for SSH connection
 class SSHFactory(ClientFactory):
 	def __init__(self):
 		self.prot = SSH()
@@ -23,16 +33,18 @@ class SSHFactory(ClientFactory):
 	def getProt(self):
 		return self.prot
 		
-
+#Data protocol
 class Data(LineReceiver):
 	def connectionMade(self):
 		reactor.connectTCP('student03.cse.nd.edu',22,ssh)
-
+	#Forward from data to ssh connection: 
 	def dataReceived(self,data):
 		ssh.getProt().sendData(data)
+	#Send data along data connection
 	def sendData(self,data):
 		self.transport.write(data)
 
+#Factory for Data connection
 class DataFactory(ClientFactory):
 	def __init__(self):
 		self.prot = Data()
@@ -41,20 +53,20 @@ class DataFactory(ClientFactory):
 	def getProt(self):
 		return self.prot
 
-
+#Command protocol
 class Command(Protocol):
 	def dataReceived(self,data):
 		#if they send "data", connect to data port
 		if data == "data\r\n":
 			reactor.connectTCP('student00.cse.nd.edu',9323,df)
-
+#Factory for command connection
 class CommandFactory(ClientFactory):
 	def buildProtocol(self,addr):
 		return Command()
 
+#Make the necessary factories
 ssh = SSHFactory()
 df = DataFactory()
-
 
 #Connect on the command port
 reactor.connectTCP('student00.cse.nd.edu',9321,CommandFactory())
